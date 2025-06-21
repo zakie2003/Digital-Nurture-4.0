ba76b52fac1a472be0255c1d4e87dc66f2111b7c
zakie khan <121384625+zakie2003@users.noreply.github.com>
2025-06-21 19:16:27 +0530

    Added Output of Excercise 7

--- Week_1/Data Structure And Algorithm/Exercise_7/Exercise_7.java
+++ Week_1/Data Structure And Algorithm/Exercise_7/Exercise_7.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Exercise_7 {
     public static double futureValue(double p,int time,double rate){
         if(time==0){
@@ -6,6 +8,11 @@ public class Exercise_7 {
         return futureValue(p*(1+rate), time-1, rate);
     }
     public static void main(String[] args) {
-        System.out.println(futureValue(1000,5, 0.05));
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the current amount:");
+        double principal = scanner.nextDouble();
+        System.out.print("Enter the number of years:");
+        int years = scanner.nextInt();
+        System.out.println("The Future Value is "+futureValue(principal, years, 0.05));
     }
 }
